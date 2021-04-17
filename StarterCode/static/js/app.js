f8108d59dbb72f687fd85c0f9b8d8f50b58c2c2b
cjvilla <CelineVillareal2020@northwestern.edu>
2021-04-17 17:58:08 -0500

    add code to buildPlot function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,7 @@
+function selectOption() { 
+    var select_subject = d3.select('#selDataset').node().value;
+ build_plot(select_subject)
+}
 
 d3.json("samples.json").then(function(data) {
     
@@ -12,18 +16,21 @@ d3.json("samples.json").then(function(data) {
     })
 });
 
-function selectOption() { 
-    var select_subject = d3.select('#selDataset').node().value;
-    buildPlot(select_subject)
-}
 
-function buildPlot(select_subject) {
+function build_plot(select_subject) {
     d3.json("samples.json").then(function(data) {
         var select_ID = select_subject
         var select_sample_values = Object.values(data.samples[select_ID].sample_values.slice(0,10).reverse());
         var select_Otu_ID = Object.values(data.samples[select_ID].otu_ids.slice(0,10).reverse());
         var select_Otu = Object.values(data.samples[select_ID].otu_labels.slice(0,10).reverse());
         
-    var demoBox = document.getElementById("sample-metadata");
-    })
-}
+    var page = document.getElementById("sample-metadata");
+    
+    
+    page.innerHTML =   `id: ${Object.values(data.metadata[select_ID])[0]}<br>
+                        ethnicity: ${Object.values(data.metadata[select_ID])[1]}<br>
+                        gender: ${Object.values(data.metadata[select_ID])[2]}<br>
+                        age: ${Object.values(data.metadata[select_ID])[3]}<br>
+                        location: ${Object.values(data.metadata[select_ID])[4]}<br>
+                        bbtype: ${Object.values(data.metadata[select_ID])[5]}<br>
+                        wfreq:${Object.values(data.metadata[select_ID])[6]}<br>`;
